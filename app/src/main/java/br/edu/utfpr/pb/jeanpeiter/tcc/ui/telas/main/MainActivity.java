765c856ce1b13db56a0547629929c9e5ee5d4abc
jpeiter <jpeiter13@gmail.com>
2021-04-06 23:54:18 -0300

    lifecycle menus

--- app/src/main/java/br/edu/utfpr/pb/jeanpeiter/tcc/ui/telas/main/MainActivity.java
+++ app/src/main/java/br/edu/utfpr/pb/jeanpeiter/tcc/ui/telas/main/MainActivity.java
@@ -1,12 +1,13 @@
 package br.edu.utfpr.pb.jeanpeiter.tcc.ui.telas.main;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
-import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.HashMap;
@@ -32,16 +33,19 @@ public class MainActivity extends AppCompatActivity implements GenericActivity {
     @Setter(AccessLevel.PRIVATE)
     BottomNavigationView bnvMenu;
 
-    private Map<Integer, Fragment> mapMenus = new HashMap<Integer, Fragment>() {{
-        put(R.id.menu_item_historico, new HistoricoFragment());
-        put(R.id.menu_item_progressso, new ProgressoFragment());
-        put(R.id.menu_item_correr, new MenuCorrerFragment());
-        put(R.id.menu_item_perfil, new Fragment());
-    }};
+    FragmentManager fragmentManager;
+
+    private final Map<Integer, Fragment> mapMenus = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mapMenus.put(R.id.menu_item_historico, new HistoricoFragment());
+        mapMenus.put(R.id.menu_item_progressso, new ProgressoFragment());
+        mapMenus.put(R.id.menu_item_correr, new MenuCorrerFragment());
+        mapMenus.put(R.id.menu_item_perfil, new Fragment());
+
+        fragmentManager = getSupportFragmentManager();
         setContentView(R.layout.activity_main);
         initViews();
     }
@@ -57,18 +61,40 @@ public class MainActivity extends AppCompatActivity implements GenericActivity {
 
         setToolbar(getSupportActionBar());
         setBnvMenu(findViewById(R.id.bnvMenu));
+        getBnvMenu().setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
         getBnvMenu().setSelectedItemId(R.id.menu_item_correr);
-        getBnvMenu().setOnNavigationItemSelectedListener((item) -> {
-            if (item.getItemId() != getBnvMenu().getSelectedItemId()) {
-                loadFragment(mapMenus.get(item.getItemId()));
-                return true;
-            }
-            return false;
-        });
     }
 
     private void loadFragment(Fragment fragment) {
         new FragmentUtils().loadFragment(this, R.id.fvContainerMain, fragment);
     }
 
+    private boolean onNavigationItemSelected(MenuItem item) {
+
+        String atualId = String.valueOf(item.getItemId());
+        String anteriorId = String.valueOf(getBnvMenu().getSelectedItemId());
+
+        if (!atualId.equals(anteriorId)) {
+            Fragment atual = mapMenus.get(item.getItemId());
+
+            if (fragmentManager.findFragmentByTag(atualId) != null) { // set o fragment existe, mostra
+                fragmentManager.beginTransaction()
+                        .show(atual)
+                        .commit();
+
+            } else { // senão, adiciona ao manager
+                fragmentManager.beginTransaction()
+                        .add(R.id.fvContainerMain, atual, atualId)
+                        .commit();
+            }
+
+            if (fragmentManager.findFragmentByTag(anteriorId) != null) { // se outro fragment está visível, oculta-o
+                fragmentManager.beginTransaction()
+                        .hide(fragmentManager.findFragmentByTag(anteriorId))
+                        .commit();
+            }
+            return true;
+        }
+        return false;
+    }
 }
